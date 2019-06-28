9272ade13fec9d41342326cd6800de51910c2a5f
Gunn <helloworld.gunn@gmail.com>
2019-06-28 11:47:30 +0800

    增加返回顶部按钮

---
+++ resources/views/components/backtop.blade.php
@@ -0,0 +1,3 @@
+<div id="back-top" class="rounded text-center bg-secondary">
+    <i class="fa fa-chevron-up text-light" aria-hidden="true"></i>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,4 +68,27 @@
         </div>
     </div>
 </div>
+
+@component('components.backtop')
+@endcomponent
+
+@endsection
+
+@section('page_js')
+<script type="text/javascript">
+$(function(){
+    $(window).scroll(function() {  //只要窗口滚动,就触发下面代码
+        var scrollt = document.documentElement.scrollTop + document.body.scrollTop; //获取滚动后的高度
+        if ( scrollt > 200 ) {  //判断滚动后高度超过200px,就显示
+            $("#back-top").fadeIn(400); //淡入
+        } else {
+            $("#back-top").stop().fadeOut(400); //如果返回或者没有超过,就淡出.必须加上stop()停止之前动画,否则会出现闪动
+        }
+    });
+
+    $("#back-top").click(function() { //当点击标签的时候,使用animate在200毫秒的时间内,滚到顶部
+        $("html, body").animate({scrollTop: "0px"}, 200);
+    });
+});
+</script>
 @endsection
